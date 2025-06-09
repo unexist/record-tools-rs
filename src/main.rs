ed5bd362df54a9d44720b7dbe943ac25cbbaab0c
Christoph Kappel <christoph@unexist.dev>
2025-06-09 12:57:14 +0200

    Added boilerplate

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+///
+/// @package heos-dial
+///
+/// @file HEOS tui
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv3.
+/// See the file LICENSE for details.
+///
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
+}
+
