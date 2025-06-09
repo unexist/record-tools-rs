ee303c6793352da12201774d580c64681d7e1f46
Christoph Kappel <christoph@unexist.dev>
2025-06-09 17:01:05 +0200

    Added initial template handling

--- src/records/create.rs
+++ src/records/create.rs
@@ -10,12 +10,42 @@
 ///
 
 use crate::Config;
-use anyhow::Result;
+use anyhow::{Context, Result};
+use std::collections::HashMap;
+use std::time::SystemTime;
+use text_template::Template;
+use time::OffsetDateTime;
+use time::macros::format_description;
 
 pub(crate) fn create(title: String, config: &Config) -> Result<()> {
     if title.is_empty() {
         anyhow::bail!("Title cannot be empty");
     }
+
+    // Load template
+    let file_path = format!("{}/{}-template.{}",
+                            config.template_dir, config.record_type, config.file_type);
+
+    let content = std::fs::read_to_string(&file_path)
+        .with_context(|| format!("Failed to open file: {}", file_path))?;
+
+    let template = Template::from(content.as_str());
+
+    let odt: OffsetDateTime = SystemTime::now().into();
+    let format = format_description!("[year]-[month]-[day]");
+    let date = odt.format(&format)?;
+
+    // Populate template
+    let mut values = HashMap::<&str, &str>::new();
+
+    values.insert("NUMBER", "1");
+    values.insert("TITLE", title.as_str());
+    values.insert("DATE", date.as_str());
+    values.insert("STATUS", "drafted");
+
+    let result = template.fill_in(&values);
+
+    println!("{}", result);
 
     println!("Created new decision record (title: {}, type: {}, superseded: {})",
              title, config.record_type, config.superseded);
