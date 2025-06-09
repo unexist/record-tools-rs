5d457cde19e25cc208983b42ca7110d5541d0ad7
Christoph Kappel <christoph@unexist.dev>
2025-06-09 13:02:02 +0200

    Updated file header

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 ///
-/// @package heos-dial
+/// @package record-tools-rs
 ///
-/// @file HEOS tui
-/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @file Main file
+/// @copyright 2025-present Christoph Kappel <christoph@unexist.dev>
 /// @version $Id$
 ///
 /// This program can be distributed under the terms of the GNU GPLv3.
