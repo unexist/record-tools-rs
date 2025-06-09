90e70033fb680b5a8785a31c77e8158df74a1ecb
Christoph Kappel <christoph@unexist.dev>
2025-06-09 15:11:04 +0200

    Updated structure and calls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 ///
 /// @package record-tools-rs
 ///
@@ -9,25 +10,52 @@
 /// See the file LICENSE for details.
 ///
 
+mod create;
+
 use clap_config_file::ClapConfigFile;
 
 #[derive(ClapConfigFile)]
 #[config_file_name = "config"]
 #[config_file_formats = "yaml,toml,json"]
 struct Config {
+    /// Record file type
     #[config_arg(default_value = "adoc")]
     file_type: String,
 
+    /// Path to Architecture Decision Records
     #[config_arg(default_value = "./architecture-decision-record")]
     adr_dir: String,
 
+    /// Path to Technical Debts Records
     #[config_arg(default_value = "./technical-debt-records")]
     tdr_dir: String,
 
+    /// Record type to create
+    #[config_arg(short = "t", accept_from = "cli_only")]
+    record_type: String,
+
+    /// Option s
+    #[config_arg(name = "s", short = "s", accept_from = "cli_only")]
+    option_s: bool,
+
+    #[config_arg(positional)]
+    commands: Vec<String>,
 }
 
 fn main() {
-    let (config, _used_file, _format) = Config::parse_info();
-
+    let (config, path, _format) = Config::parse_info();
+    
+    println!("Loaded config from: {:?}", path.unwrap_or_default());
     println!("Config: {:?}", config);
+    println!("Command: {:?}", config.commands);
+
+    if !config.commands.is_empty() {
+        match config.commands[0].as_str() {
+            "new" => {
+                create::create(config.commands[1..].join(" ").into(), &config);
+            },
+            _ => {}
+        }
+
+    }
 }
