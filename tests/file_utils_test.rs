1c011bbe00103dbc7adcc1d3dbb7dd88aa211937
Christoph Kappel <christoph@unexist.dev>
2025-06-10 15:49:30 +0200

    Fixed path to test file

--- tests/file_utils_test.rs
+++ tests/file_utils_test.rs
@@ -9,16 +9,16 @@
 /// See the file LICENSE for details.
 ///
 
-#[path = "../src/records/mod.rs"]
-mod records;
+#[path = "../src/records/file_utils.rs"]
+mod file_utils;
 
 #[test]
 fn should_find_next_number() {
     let path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"),
                        "example/src/site/asciidoc/architecture-decision-records");
-    
-    let number = records::file_utils::find_next_val(&*path);
 
+    let number = file_utils::find_next_val(&*path);
+    
     assert!(number.is_ok());
     assert_eq!(number.unwrap(), 1);
 }
