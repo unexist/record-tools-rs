a0c4efffab11aafe0f17b79ee9146e45672ed935
Christoph Kappel <christoph@unexist.dev>
2025-06-11 15:45:08 +0200

    Extracted helper method

--- tests/file_utils_test.rs
+++ tests/file_utils_test.rs
@@ -16,18 +16,27 @@ use std::fs::File;
 use std::path::Path;
 use proptest::prelude::*;
 use tempfile::TempDir;
+use anyhow::Result;
+
+fn create_n_records(n: u16) -> Result<TempDir> {
+    let temp_dir = TempDir::new()?;
+    
+    for i in 1..n {
+        File::create(
+            Path::new(&temp_dir.path().join(format!("{:04}-test-adr.adoc", i))))
+            .expect("Can't create file");
+    }
+    
+    Ok(temp_dir)
+}
 
 proptest! {
     #![proptest_config(ProptestConfig::with_cases(5))]
     #[test]
     fn should_find_next_file_number(n in 1u16..20) {
-        let temp_dir = TempDir::new()?;
-        
-        for i in 1..n {
-            File::create(
-                Path::new(&temp_dir.path().join(format!("{:04}-test-adr.adoc", i))))
-                .expect("Can't create file");
-        }
+        let temp_dir = create_n_records(n)
+            .expect("Can't create temp dir");
+
 
         let number = file_utils::find_next_num(temp_dir.path().to_str()
             .expect("Can't get path of temp dir"));
