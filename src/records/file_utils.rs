d0349fd100fd3f214b1d6ac534f13723f35e60d8
Christoph Kappel <christoph@unexist.dev>
2025-06-11 15:45:24 +0200

    Started work on field extractor

--- src/records/file_utils.rs
+++ src/records/file_utils.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 ///
 /// @package record-tools-rs
 ///
@@ -9,7 +10,8 @@
 /// See the file LICENSE for details.
 ///
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use regex::Regex;
 
 pub fn find_next_num(path: &str) -> Result<u16> {
     let last_entry = std::fs::read_dir(path)?
@@ -19,11 +21,24 @@ pub fn find_next_num(path: &str) -> Result<u16> {
     
     if let Some(entry) = last_entry {
         let number = entry.file_name().to_str()
-            .with_context(|| format!("Could not convert {:?} to string", entry.file_name()))?
+            .with_context(|| format!("Couldn't convert {:?} to string", entry.file_name()))?
             .chars().take(4).collect::<String>();
         
         return number.parse::<u16>().map_err(anyhow::Error::from).map(|i| i + 1);
     }
     
     Ok(1)
+}
+
+fn extract_field(path: &str, name: &str) -> Result<String> {
+    let content = std::fs::read_to_string(&path)
+        .with_context(|| format!("Failed to open template file: {}", path))?;
+
+    let re = Regex::new(&*format!("{}:[ |](.*)", name))?;
+
+    re.captures(&content).map(|caps| caps.extract())
+}
+
+pub fn get_title(&self) -> Option<&String> {
+    self.meta.get("title")
 }
