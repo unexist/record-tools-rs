94cf0f63796019802f17285861d564dc11f4bd6a
Christoph Kappel <christoph@unexist.dev>
2026-05-29 10:52:52 +0200

    Replaced logging with env logger

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,35 @@
 /// See the file LICENSE for details.
 ///
 
+/// Records module
 mod records;
+/// Config module
 mod config;
+/// Log facility
+mod logger;
 
 use std::path::Path;
 use std::process::exit;
 use anyhow::{bail, Result};
+use log::{error, info};
 use crate::config::Config;
 
+/// Print version info
+fn print_version() {
+    info!("{} {} - Copyright (c) 2025-present {}",
+        env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_AUTHORS"));
+    info!("Released under the GNU GPLv3");
+}
+
+/// Sanity-check configuration
+///
+/// # Arguments
+///
+/// * `config` - Config values read either from args or config file
+///
+/// # Returns
+///
+/// A [`Result`] with either [`unit`] on success or otherwise [`anyhow::Error`]
 fn sanity_checks(config: &Config) -> Result<()> {
     if !Path::new(config.template_dir.as_str()).exists() {
         bail!("Template directory {} does not exist", config.template_dir);
@@ -31,6 +52,15 @@ fn sanity_checks(config: &Config) -> Result<()> {
     Ok(())
 }
 
+/// Handle commands
+///
+/// # Arguments
+///
+/// * `config` - Config values read either from args or config file
+///
+/// # Returns
+///
+/// A [`Result`] with either [`unit`] on success or otherwise [`anyhow::Error`]
 fn handle_command(config: &Config) -> Result<()> {
     if !config.commands.is_empty() {
         let subcmd = config.commands[0].as_str();
@@ -50,21 +80,32 @@ fn handle_command(config: &Config) -> Result<()> {
     Ok(())
 }
 
-fn main() {
+/// Main function
+///
+/// # Returns
+///
+/// A [`Result`] with either [`unit`] on success or otherwise [`anyhow::Error`]
+fn main() -> Result<()> {
     let (config, path, _format) = Config::parse_info();
 
-    println!("Loaded config from: {:?}", path.unwrap_or_default());
-    println!("Config: {:?}", config);
-    println!("Command: {:?}", config.commands);
+    logger::init(&config)?;
+
+    print_version();
+
+    info!("Loaded config from: {:?}", path.unwrap_or_default());
+    info!("Config: {:?}", config);
+    info!("Command: {:?}", config.commands);
 
     if let Err(e) = sanity_checks(&config) {
-        eprintln!("Error: {}", e);
+        error!("Error: {}", e);
 
         exit(1);
     }
 
     // Run actual command
     if let Err(e) = handle_command(&config) {
-        eprintln!("Error: {}", e);
+        error!("Failed to handle command: {}", e);
     }
+
+    Ok(())
 }
