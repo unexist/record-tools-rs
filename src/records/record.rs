e44e73647d63d392163f37f69a439c538a926035
Christoph Kappel <christoph@unexist.dev>
2026-05-31 12:42:09 +0200

    Added non-working WIP

--- src/records/record.rs
+++ src/records/record.rs
@@ -1,3 +1,4 @@
+use std::cell::OnceCell;
 ///
 /// @package record-tools-rs
 ///
@@ -10,72 +11,98 @@
 ///
 
 use crate::Config;
-use crate::records::file_utils;
 use anyhow::{Context, Result, bail};
 use slugify::slugify;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::time::SystemTime;
-use derive_builder::Builder;
 use text_template::Template;
 use time::OffsetDateTime;
 use time::macros::format_description;
 use log::info;
 use stdext::default::default;
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub(crate) struct Record {
-    pub(crate) path: String,
+    pub(crate) content: String,
+    pub(crate) target_path: String,
 }
 
-#[derive(Builder)]
-#[builder(name = "RecordBuilder", build_fn(skip))]
-pub(crate) struct RecordBuilderSeed {
-
+impl Record {
+    pub(crate) fn write(self: Self, _path: &str) -> Result<()> {
+        Ok(())
+    }
 }
 
-impl RecordBuilder {
+pub(crate) struct RecordBuilder<'a> {
+    pub(crate) template: Template<'a>,
+    pub(crate) values: HashMap<String, String>,
+    pub(crate) number: u16,
+    pub(crate) title: String,
+    pub(crate) date: String,
+}
+
+impl TryFrom<&Config> for RecordBuilder<'_> {
+    type Error = anyhow::Error;
+
+    fn try_from(config: &Config) -> Result<Self> {
+        let content= std::fs::read_to_string(config.get_default_template_path()?)?;
+
+        Ok(RecordBuilder {
+            template: Template::from(content.clone().as_str()),
+            values: Default::default(),
+            number: find_next_num(&PathBuf::from(&config.get_current_path()?))?,
+            title: Default::default(),
+            date: Default::default(),
+        })
+    }
+}
+
+impl<'a> RecordBuilder<'a> {
+    pub(crate) fn set(&mut self, key: String, value: String) -> &mut RecordBuilder<'a> {
+        self.values.insert(key, value);
+
+        self
+    }
+
+    pub(crate) fn set_number(&mut self, number: u32) -> &mut RecordBuilder<'a> {
+        self.values.insert(String::from("NUMBER"), number.to_string());
+
+        self
+    }
+
+    pub(crate) fn set_title(&mut self, title: String) -> &mut RecordBuilder<'a> {
+        self.values.insert(String::from("TITLE"), title);
+
+        self
+    }
+
+    pub(crate) fn set_date_now(&mut self) -> &mut RecordBuilder<'a> {
+        let odt: OffsetDateTime = SystemTime::now().into();
+        let format = format_description!("[year]-[month]-[day]");
+        let date = odt.format(&format).expect("This date format should never fail");
+
+        self.values.insert(String::from("DATE"), date);
+
+        self
+    }
+
     pub(crate) fn build(&mut self) -> Result<Record> {
+
         Ok(Record {
-            ..default()
+           content: self.template.fill_in(&self.values).to_string(),
+            target_path:  format!("{}/{:04}-{}.{}",
+                config.get_current_path()?,
+                next_num,
+                slugify!(&*title),
+                config.file_type),
         })
     }
 }
 
 pub(crate) fn execute(title: String, config: &Config) -> Result<()> {
-    if title.is_empty() {
-        bail!("Title cannot be empty");
-    }
-
-    // Load template
-    let source_path = format!("{}/{}-template.{}",
-        config.template_dir, config.record_type, config.file_type
-    );
-
-    let content = std::fs::read_to_string(&source_path)
-        .with_context(|| format!("Failed to open template file: {}", source_path))?;
-
-    let template = Template::from(content.as_str());
-
-    // Get number
-    let next_num = file_utils::find_next_num(&PathBuf::from(&config.get_current_path()?))?;
-    let next_num_str = format!("{}", next_num);
-
-    // Get time
-    let odt: OffsetDateTime = SystemTime::now().into();
-    let format = format_description!("[year]-[month]-[day]");
-    let date = odt.format(&format)?;
-
-    // Populate template
-    let mut values = HashMap::<&str, &str>::new();
-
-    values.insert("NUMBER", next_num_str.as_str());
-    values.insert("TITLE", title.as_str());
-    values.insert("DATE", date.as_str());
-    values.insert("STATUS", "drafted");
-
     let result = template.fill_in(&values);
 
     // Write template
@@ -103,4 +130,21 @@ pub(crate) fn execute(title: String, config: &Config) -> Result<()> {
     );
 
     Ok(())
+}
+
+pub(crate) fn find_next_num(path: &Path) -> Result<u16> {
+    let last_entry = std::fs::read_dir(path)?
+        .flatten()
+        .filter(|f| f.metadata().unwrap().is_file())
+        .max_by_key(|x| x.file_name());
+
+    if let Some(entry) = last_entry {
+        let number = entry.file_name().to_str()
+            .with_context(|| format!("Couldn't convert {:?} to string", entry.file_name()))?
+            .chars().take(4).collect::<String>();
+
+        return number.parse::<u16>().map_err(anyhow::Error::from).map(|i| i + 1);
+    }
+
+    Ok(1)
 }
