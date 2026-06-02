b949abbe30bd7cc7559ff43501245edeb5492073
Christoph Kappel <christoph@unexist.dev>
2026-06-02 15:37:36 +0200

    Migrated to use the record builder

--- src/records/create.rs
+++ src/records/create.rs
@@ -11,75 +11,41 @@
 
 use crate::Config;
 use anyhow::{Context, Result, bail};
-use slugify::slugify;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
-use std::time::SystemTime;
-use text_template::Template;
-use time::OffsetDateTime;
-use time::macros::format_description;
 use log::info;
 use crate::records::record;
+use crate::records::record::{RecordBuilder, RecordAttributes};
 
-pub(crate) fn execute(title: String, config: &Config) -> Result<()> {
+/// Execute command
+///
+/// # Arguments
+///
+/// * `config` - Config values read either from args or config file
+/// * `attrs` - Record attributes
+///
+/// # Returns
+///
+/// A [`Result`] with either [`unit`] on success or otherwise [`anyhow::Error`]
+pub(crate) fn execute(config: &Config, attrs: &RecordAttributes) -> Result<()> {
+    let title = attrs.get("title").unwrap_or(DEFAULT_TITLE);
+
     if title.is_empty() {
         bail!("Title cannot be empty");
     }
 
-    // Load template
-    let source_path = format!("{}/{}-template.{}",
-        config.template_dir, config.record_type, config.file_type
-    );
-
-    let content = std::fs::read_to_string(&source_path)
-        .with_context(|| format!("Failed to open template file: {}", source_path))?;
-
-    let template = Template::from(content.as_str());
-
-    // Get number
-    let next_num = record::find_next_num(&PathBuf::from(&config.get_current_path()?))?;
-    let next_num_str = format!("{}", next_num);
-
-    // Get time
-    let odt: OffsetDateTime = SystemTime::now().into();
-    let format = format_description!("[year]-[month]-[day]");
-    let date = odt.format(&format)?;
-
-    // Populate template
-    let mut values = HashMap::<&str, &str>::new();
-
-    values.insert("NUMBER", next_num_str.as_str());
-    values.insert("TITLE", title.as_str());
-    values.insert("DATE", date.as_str());
-    values.insert("STATUS", "drafted");
-
-    let result = template.fill_in(&values);
-
-    // Write template
-    let target_path = format!("{}/{:04}-{}.{}",
-        config.get_current_path()?,
-        next_num,
-        slugify!(&*title),
-        config.file_type
-    );
+    let record = RecordBuilder::try_from(config)?
+        .set_number(1)
+        .set_title(title)
+        .set_date_now()
+        .merge(attrs)
+        .build()?;
 
     if config.dry_run {
-        println!("Dry-run: {}:\n{}", target_path, result);
+        println!("Dry-run: {}:\n{}", record.target_path, record.content);
     } else {
-        let mut file = File::create_new(&target_path)
-            .with_context(|| format!("Failed to create new file: {}", target_path))?;
-
-        file.write_all(result.to_string().as_bytes())
-            .with_context(|| format!("Failed to write to file: {}", target_path))?;
+        record.write()?;
     }
-
-    info!("Created new decision record {} (dry-run: {}, superseded: {})",
-        target_path,
-        config.dry_run,
-        !config.superseded.is_empty()
-    );
 
     Ok(())
 }
