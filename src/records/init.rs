f64b7c193fa52874e4787874e154b2657fd80693
Christoph Kappel <christoph@unexist.dev>
2026-06-02 15:37:54 +0200

    Added method header and updated attribute handling

--- src/records/init.rs
+++ src/records/init.rs
@@ -12,13 +12,26 @@
 
 use anyhow::Result;
 use crate::Config;
-use crate::records::record::RecordBuilder;
+use crate::records::record::{RecordBuilder, RecordAttributes};
 
-pub(crate) fn execute(title: String, config: &Config) -> Result<()> {
+/// Execute command
+///
+/// # Arguments
+///
+/// * `config` - Config values read either from args or config file
+/// * `attrs` - Record attributes
+///
+/// # Returns
+///
+/// A [`Result`] with either [`unit`] on success or otherwise [`anyhow::Error`]
+pub(crate) fn execute(config: &Config, attrs: &RecordAttributes) -> Result<()> {
+    let title = attrs.get("title").unwrap_or(DEFAULT_TITLE);
+
     let record = RecordBuilder::try_from(config)?
         .set_number(1)
         .set_title(title)
         .set_date_now()
+        .merge(attrs)
         .build()?;
 
     record.write()?;
