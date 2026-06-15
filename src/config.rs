97ee30c146a3caa9175159a0322d79d08cf6152b
Christoph Kappel <christoph@unexist.dev>
2026-06-15 10:58:53 +0200

    Updated confog handler for new config format

--- src/config.rs
+++ src/config.rs
@@ -23,7 +23,7 @@ pub(crate) struct Config {
     pub(crate) loglevel: String,
 
     /// Print debugging messages
-    #[config_arg(short = 'd', default_value = false, accept_from = "cli_only")]
+    #[config_arg(short = 'D', default_value = false, accept_from = "cli_only")]
     pub(crate) debug: bool,
 
     /// Print no system messages at all
@@ -31,15 +31,19 @@ pub(crate) struct Config {
     pub(crate) quiet: bool,
 
     /// Record file type
-    #[config_arg(default_value = "adoc")]
-    pub(crate) file_type: String,
+    #[config_arg(short = 'd', name = "doc_type", default_value = "adoc")]
+    pub(crate) doc_type: String,
 
     /// Path to templates
     #[config_arg(default_value = "./templates")]
     pub(crate) template_dir: String,
 
+    /// List of known document types
+    #[config_arg(name = "doc_types", accept_from = "config_only")]
+    pub(crate) doc_types: Vec<HashMap<String, String>>,
+
     /// List of known record types
-    #[config_arg(name = "record_type", accept_from = "config_only")]
+    #[config_arg(name = "record_types", accept_from = "config_only")]
     pub(crate) record_types: Vec<HashMap<String, String>>,
 
     /// Record type to create
@@ -51,7 +55,7 @@ pub(crate) struct Config {
     pub(crate) username: String,
 
     /// Record type to create
-    #[config_arg(short = 'T', default_value = "No title given", accept_from = "cli_only")]
+    #[config_arg(short = 'T', default_value = record_builder::DEFAULT_TITLE, accept_from = "cli_only")]
     pub(crate) title: String,
 
     /// Edit record after creation
@@ -104,12 +108,28 @@ impl Config {
     pub(crate) fn get_default_template_path(self: &Self) -> Result<PathBuf> {
         for record_type in self.record_types.iter() {
             if Some(&self.record_type) == record_type.get("name") {
-                let template_name = record_type.get("default_template_name").context("No default template found")?;
+                let template_name = record_type.get("template_name").context("No default template found")?;
 
-                return Ok(PathBuf::from(format!("{}/{}.{}", self.template_dir, template_name, self.file_type)))
+                return Ok(PathBuf::from(format!("{}/{}.{}", self.template_dir, template_name, self.doc_type)))
             }
         }
 
         bail!("No default template found");
+    }
+
+    /// Get link format according to type
+    ///
+    /// # Returns
+    ///
+    /// A [`Result`] with either [`String`] on success or otherwise [`anyhow::Error`]
+    #[allow(dead_code)]
+    pub(crate) fn get_link_format(self: &Self) -> Result<String> {
+        for record_type in self.record_types.iter() {
+            if Some(&self.record_type) == record_type.get("name") {
+                return Ok(String::from(record_type.get("link_format").context("No link format found")?));
+            }
+        }
+
+        bail!("No record type found");
     }
 }
