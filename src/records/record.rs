301f0ec08e42ae4f48e7ab03090c6cc6e3b883cb
Christoph Kappel <christoph@unexist.dev>
2026-09-25 17:36:02 +0200

    Reformatted file

--- src/records/record.rs
+++ src/records/record.rs
@@ -11,9 +11,9 @@
 
 use anyhow::Result;
 use asciidocr::{backends::htmls::render_htmlbook, parser::Parser, scanner::Scanner};
-use std::{fs::File, path::PathBuf};
+use log::{debug, info};
 use std::io::Write;
-use log::{info, debug};
+use std::{fs::File, path::PathBuf};
 
 #[derive(Debug)]
 pub(crate) struct Record {
@@ -22,17 +22,15 @@ pub(crate) struct Record {
 }
 
 impl Record {
-
     /// Write record to disk
-     ///
-     /// # Returns
-     ///
-     /// A [`Result`] with either [`Record`] on success or otherwise [`anyhow::Error`]
+    ///
+    /// # Returns
+    ///
+    /// A [`Result`] with either [`Record`] on success or otherwise [`anyhow::Error`]
     pub(crate) fn write(self) -> Result<()> {
         debug!("Creating record `{}`", self.target_path);
 
-        File::create_new(&self.target_path)?
-            .write_all(self.content.to_string().as_bytes())?;
+        File::create_new(&self.target_path)?.write_all(self.content.to_string().as_bytes())?;
 
         info!("Wrote record `{}`", self.target_path);
 
@@ -40,20 +38,22 @@ impl Record {
     }
 
     /// Write record as html to disk
-     ///
-     /// # Returns
-     ///
-     /// A [`Result`] with either [`()`] on success or otherwise [`anyhow::Error`]
+    ///
+    /// # Returns
+    ///
+    /// A [`Result`] with either [`()`] on success or otherwise [`anyhow::Error`]
     pub(crate) fn write_html(self, path: PathBuf, css: &String) -> Result<()> {
         if let Ok(asg) = Parser::new(path).parse(Scanner::new(&self.content)) {
             if let Ok(html) = render_htmlbook(&asg) {
                 debug!("HTML out: {}", html);
 
                 let html_file = format!("{}.html", &self.target_path);
-                let html_content = html.to_string().replace("</head>", &format!(r#"<style type="text/css">{}</style></head>"#, css));
+                let html_content = html.to_string().replace(
+                    "</head>",
+                    &format!(r#"<style type="text/css">{}</style></head>"#, css),
+                );
 
-                File::create_new(html_file)?
-                    .write_all(html_content.as_bytes())?;
+                File::create_new(html_file)?.write_all(html_content.as_bytes())?;
             }
         }
 
